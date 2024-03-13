2ad3f84259a6d00c3e8aa8914f220b7100ecc673
Oriol Deprius <a2207810@institutmontilivi.cat>
2024-03-13 12:24:19 +0100

    Update Program
    Afegits alguns menus i switch

--- BotigaCistellaPOO/BotigaCistellaPOO/Program.cs
+++ BotigaCistellaPOO/BotigaCistellaPOO/Program.cs
@@ -4,56 +4,169 @@
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.CursorVisible = false;
-            
+            char respostaMenuInicial;
+            char respostaMenuAdministrarBotiga;
+            string contrasenyaIntroduida;
+            string contrasenyaAdmin = "xxx123xxx";
 
+            do
+            {
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine(MenuInicial());
+                respostaMenuInicial = Console.ReadKey().KeyChar;
+                switch (respostaMenuInicial)
+                {
+                    case '1':
+
+                        break;
+                    case '2':
+                        bool contrasenyaCorrecta = false;
+                        do
+                        {
+                            Console.Clear();
+                            Console.Write("Introdueix la contrasenya: ");
+                            contrasenyaIntroduida = Console.ReadLine();
+
+                            if (contrasenyaIntroduida == contrasenyaAdmin)
+                            {
+                                Console.WriteLine("Contrasenya correcta.");
+                                contrasenyaCorrecta = true;
+                                Thread.Sleep(1000);
+                                Console.Clear();
+                                for (int i = 3; i >= 1; i--)
+                                {
+                                    Console.Write($"Entrant a l'administració de la botiga en... ");
+                                    Console.WriteLine(i);
+                                    Thread.Sleep(1000);
+                                    Console.Clear();
+                                }
+
+                                Console.WriteLine(MenuAdministrarBotiga());
+                                respostaMenuAdministrarBotiga = Console.ReadKey().KeyChar;
+
+                                if (respostaMenuAdministrarBotiga == '1')
+                                    SwitchBotiga(respostaMenuAdministrarBotiga);
+
+                                if (respostaMenuAdministrarBotiga == '2')
+                                    SwitchProducte(respostaMenuAdministrarBotiga);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Contrasenya incorrecta.");
+                                Thread.Sleep(2000);
+                            }
+                        }
+                        while (!contrasenyaCorrecta);
+                        break;
+                    case '0':
+                        Console.Clear();
+                        Console.WriteLine("Adeu!");
+                        break;
+                }
+            }
+            while (respostaMenuInicial != '0');
         }
         static string MenuInicial()
         {
-            string menu = "╔══════════════════════════════════╗" +
-                          "║*-*-*-*-*-TRIA UNA OPCIÓ-*-*-*-*-*║" +
-                          "╠══════════════════════════════════╣" +
-                          "║                                  ║" +
-                          "║    1.  Comprar Productes         ║" +
-                          "║                                  ║" +
-                          "║    2.  Administrar Botiga        ║" +
-                          "║                                  ║" +
-                          "╚══════════════════════════════════╝";
+            string menu = "╔══════════════════════════════════╗\n" +
+                          "║*-*-*-*-*-TRIA UNA OPCIÓ-*-*-*-*-*║\n" +
+                          "╠══════════════════════════════════╣\n" +
+                          "║                                  ║\n" +
+                          "║    1.  Comprar Productes         ║\n" +
+                          "║                                  ║\n" +
+                          "║    2.  Administrar Botiga        ║\n" +
+                          "║                                  ║\n" +
+                          "╚══════════════════════════════════╝\n";
             return menu;
         }
         static string MenuAdministrarBotiga()
         {
-            string menu = "╔══════════════════════════════════╗" +
-                          "║*-*-*-*-*-TRIA UNA OPCIÓ-*-*-*-*-*║" +
-                          "╠══════════════════════════════════╣" +
-                          "║                                  ║" +
-                          "║          1.  Botiga              ║" +
-                          "║                                  ║" +
-                          "║          2.  Producte            ║" +
-                          "║                                  ║" +
-                          "╚══════════════════════════════════╝";
-                          
+            string menu = "╔══════════════════════════════════╗\n" +
+                          "║*-*-*-*-*-TRIA UNA OPCIÓ-*-*-*-*-*║\n" +
+                          "╠══════════════════════════════════╣\n" +
+                          "║                                  ║\n" +
+                          "║          1.  Botiga              ║\n" +
+                          "║                                  ║\n" +
+                          "║          2.  Producte            ║\n" +
+                          "║                                  ║\n" +
+                          "╚══════════════════════════════════╝\n";
             return menu;
         }
         static string MenuBotiga()
         {
-            string menu = "╔══════════════════════════════════════╗" +
-                          "║*-*-*-*-*-*-TRIA UNA OPCIÓ-*-*-*-*-*-*║" +
-                          "╠══════════════════════════════════════╣" +
-                          "║                                      ║" +
-                          "║  1. Crear Botiga                     ║" +
-                          "║  2. Veure Botiga                     ║" +
-                          "║  3. Canviar nom Botiga               ║" +
-                          "║  4.          ║" +
-                          "║  5.                    ║" +
-                          "║  6. Ordenar Productes (NOM)          ║" +
-                          "║  7. Ordenar Productes (PREU)         ║" +
-                          "║  8.     ║";
+            string menu = "╔══════════════════════════════════════╗\n" +
+                          "║*-*-*-*-*-*-TRIA UNA OPCIÓ-*-*-*-*-*-*║\n" +
+                          "╠══════════════════════════════════════╣\n" +
+                          "║                                      ║\n" +
+                          "║  1. Crear Botiga                     ║\n" +
+                          "║  2. Veure Botiga                     ║\n" +
+                          "║  3. Canviar nom Botiga               ║\n" +
+                          "║  4.          ║\n" +
+                          "║  5.                    ║\n" +
+                          "║  6. Ordenar Productes (NOM)          ║\n" +
+                          "║  7. Ordenar Productes (PREU)         ║\n" +
+                          "║  8.     ║\n";
 
             return menu;
+        }
+        static void SwitchBotiga(char opcio)
+        {
+            Console.Clear();
+            Console.WriteLine(MenuBotiga());
+            char respostaMenuBotiga = Console.ReadKey().KeyChar;
+            switch (respostaMenuBotiga)
+            {
+                case '1':
+                    break;
+                case '2':
+                    break;
+                case '3':
+                    break;
+                case '4':
+                    break;
+                case '5':
+                    break;
+                case '6':
+                    break;
+                case '7':
+                    break;
+                case '8':
+                    break;
+            }
+        }
+        static string MenuProducte()
+        {
+            string menu = "╔";
+
+            return menu;
+        }
+        static void SwitchProducte(char opcio)
+        {
+            Console.Clear();
+            Console.WriteLine(MenuProducte());
+            char respostaMenuProducte = Console.ReadKey().KeyChar;
+            switch (respostaMenuProducte)
+            {
+                case '1':
+                    break;
+                case '2':
+                    break;
+                case '3':
+                    break;
+                case '4':
+                    break;
+                case '5':
+                    break;
+                case '6':
+                    break;
+                case '7':
+                    break;
+                case '8':
+                    break;
+            }
         }
     }
 }
